5c78365a04d877be49346437263ea456b3480d54
BadPerson <1249750733@qq.com>
2018-09-18 16:30:39 +0800

    修改BUG

--- src/command/config.php
+++ src/command/config.php
@@ -1,11 +1,4 @@
 <?php
-// +----------------------------------------------------------------------
-// | TopThink [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2015 http://www.topthink.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: zhangyajun <448901948@qq.com>
-// +----------------------------------------------------------------------
 
 \think\Console::addDefaultCommands([
     "BadpersonOrder\\command\\CreateOrder",
